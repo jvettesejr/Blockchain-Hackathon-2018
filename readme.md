9c69e3bf7ffd91d415ef1c90f6290eae6b04c90c
Francesco Alessandro Stefano Mikulis-Borsoi <francescomikulis@gmail.com>
2018-10-20 02:37:09 -0400

    Francesco: added fields to post, cleaned up code and now able to automate multiple functions.

--- readme.md
+++ readme.md
@@ -1,1 +1,32 @@
-This is a README.md
+# Git-LBRY ![CI status](https://img.shields.io/badge/build-passing-brightgreen.svg)
+
+Git_LBRY is a version control system that utilizes the LBRY platform.
+
+## Contributors
+* Kristian Comer            `kc1142@wildcats.unh.edu`
+* Francesco Mikulus-Borsoi  `fam1001@wildcats.unh.edu`
+* Max Miller                `mam1101@wildcats.unh.edu`
+* Jason Vettese             `jmv1012@wildcats.unh.edu`
+
+## Installation
+Please visit LBRY link to configure your access to the platform
+https://github.com/lbryio/lbry/blob/master/INSTALL.md
+
+### Requirements
+* MacOS, Linux, Windows
+* Python 3.7
+
+## Usage
+The program can be run with using the template:
+```
+git-lbry [command] [flag] [arguments]
+```
+Some sample commands:
+```
+git-lbry commit this.py # commits the file this.py
+git-lbry status         # Shows the version status
+git-lbry -h             # Displays a help menu
+```
+## Contributing
+This platform was created as part of the UNH Blockchain Hackathon. Any changes are welcome.
+
--- run_requests.py
+++ run_requests.py
@@ -1,61 +1,53 @@
 import os
 import requests
+from typing import Dict
 
 
 initial_wallet_address = "bUUH24gcxF2rWwcunaWFfwoTR5dDFfYjnt"
 
 tx_id = "e2d4c71097d290062111d4eee2a1e65464f0abcbac0e93bb7eae85c9ee1eb52d"
 
-
 url = "http://localhost:5279"
 
+
 def test_post():
-    result = requests.post(
-        url,
-        json={"method":"resolve","params":{"uri":"what"}},
-        headers={"Content-Type": "application/json"}
-    )
-    print("TEST!!")
-    print(result.status_code)
-    print(result.json())
+    print("\nAnalyzing connection to blockchain...")
+    json = post_information(method="resolve", params={"uri":"what"})
+    print(json)
+
 
 def post_file(repository, file_name):
-    print("CONTENT IS THE FOLLOWING")
-
     current_dir_path = os.path.dirname(os.path.realpath(__file__))
     real_path = os.path.join(current_dir_path, file_name)
 
-    print(real_path)
-
-    file_data = {
-        "method":"publish",
-        "params": {
-            "name": repository,
-            "file_path": real_path,
-            "bid": 1.0,
-            "metadata":  {
-                "description": "Testing out a file upload",
-                "title": "Zipped file upload",
-                "author": "File created by the Three Comma Club",
-                "language": "en",
-                "license": "TCC",
-                "nsfw": False
-            }
-        }
+    metadata = {
+        "description": "Testing out a file upload",
+        "title": "Zipped file upload",
+        "author": "File created by the Three Comma Club",
+        "language": "en",
+        "license": "TCC",
+        "nsfw": False
+    }
+    params = {
+        "name": repository,
+        "file_path": real_path,
+        "bid": 1.0,
+        "metadata": metadata,
     }
 
-    result = requests.post(
-        url,
-        json=file_data,
-        headers={"Content-Type": "application/json"}
-    )
+    json = post_information(method="publish", params=params)
 
-    print(result.json())
-
+    print("\nUploaded the file with a post, a bid of 1 LBC")
+    print(json)
     
 
 def get_files(repository):
-    pass
+    # CLI: get(uri=repository)
+    json = post_information(method="get", params={"uri": repository})
+    print("\nRetrived the files and they will be downloaded at:")
+    print(json["result"]['download_path'])
+    print("\nThe following data was returned:")
+    print(json)
 
 def generate_wallet_address():
     """
@@ -69,19 +61,36 @@ def generate_wallet_address():
     pass
 
 def get_wallet_balance():
+    print("\nRetrieving wallet balance")
+    print(post_information(method = "wallet_balance"))
+
+
+def post_information(method: str, params: Dict = dict()) -> Dict:
+    json = dict()
+    if not method:
+        return {}
+    if not(params):
+        json = {"method": method}
+    else:
+        json = {
+            "method": method,
+            "params": params,
+        }
     result = requests.post(
         url,
-        json={"method":"wallet_balance"},
+        json = json,
         headers={"Content-Type": "application/json"}
     )
-    print(result.json())
-
-
+    if result.status_code != 200:
+        return {}
+    else:
+        return result.json()
 
 
 if __name__ == "__main__":
     # test_post()
     get_wallet_balance()
+    # get_files("hackathon-lbry-threecommaclub")
 
     # post_file(repository = "hackathon-lbry-threecommaclub", file_name = "README.zip")
 
